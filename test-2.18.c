1edc7bcd5a4017673c0cb31ac952ea9978c76f0b
xi-xiansheng <xi.xiansheng@outlook.com>
2021-02-18 13:30:27 +0800

    第一次测试

---
+++ test-2.18.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	printf("#########是否加入#########")
+	printf("##1.加入       2.不加入##"）
+	int i = 0;
+	scanf("%d\n,&i")
+		switch (i)
+	{
+	case1:printf("欢迎加入\n"); 
+		berak;
+	case0:printf("很遗憾、n");
+		berak;
+	default:printf("输入错误\n")
+	}
+	return 0;
+}
